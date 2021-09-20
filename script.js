321d57517f90bcf0bb3d08a59f722c907b500b6f
Blawk <stijn.timmerman@student.nhlstenden.com>
2021-09-20 14:36:01 +0200

    init()

---
+++ script.js
@@ -0,0 +1,20 @@
+/* Constants */
+
+/* app's state*/
+
+let board;
+
+/* cached element references */
+
+/* event listeners */
+
+/* functions */ 
+
+function init(){
+    board = [
+        '', '', '',
+'', '', '',
+'', '', ''
+];
+};
+ init();

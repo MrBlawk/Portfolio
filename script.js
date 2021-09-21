ad2872c07ef7ef490bc2136339dfb155a0581749
Blawk <stijn.timmerman@student.nhlstenden.com>
2021-09-21 11:30:59 +0200

    Functionele turns

--- script.js
+++ script.js
@@ -16,20 +16,20 @@ const winningCombos = [
 /* app's state*/
 
 let board;
-
 let turn = 'X';
-
 let win;
 
-win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
+
 
 /* cached element references */
 const squares = Array.from(document.querySelectorAll('#board div'));
 
-const messages = document.querySelector('h2');
+
 
 /* event listeners */
 document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
+document.getElementById('reset-button').addEventListener('click', init);
 
 
 /* functions */ 
@@ -40,11 +40,10 @@ function getWinner()
 
     winningCombos.forEach(function(combo,index){
 
-        if(board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]])
-        winner = board[combo[0]]
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
     });
 
-    return winner;
+    return winner ? winner : board.includes('') ? null : 'T';
     
 };
 
@@ -81,21 +80,19 @@ function init(){
 ];
 render();
 };
+
+
+
+
+function render() {
+    board.forEach(function(mark, index) {
+    //this moves the value of the board item into the squares[idx]
+    squares[index].textContent = mark;
+    });
+    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    };
+
 init();
 
- function render(){
- 
-
-        board.forEach(function(mark, index){
-        squares[index].textContent = mark;
-        //console.log(mark, index);
-        console.log(winner)
-        
-
-        });
-
-        messages.textContent = win ? `${win} 'wins the game!'` : `It's ${turn} 's turn!`;
-
- };
 
  

64d5381a5d19256dde89b1983c56fbfc8bfd6178
Tran <RosendahlTran@gmail.com>
2021-09-21 11:20:38 +0200

    Push update, win conditite

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
 /* Constants */
 
+const winningCombos = [
+
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6],
+
+];
+
 /* app's state*/
 
 let board;
 
 let turn = 'X';
 
+let win;
+
+win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
+
 /* cached element references */
 const squares = Array.from(document.querySelectorAll('#board div'));
+
+const messages = document.querySelector('h2');
 
 /* event listeners */
 document.getElementById('board').addEventListener('click', handleTurn);
 
 
 /* functions */ 
+function getWinner()
+{
+    //just stub it up
+    let winner = null;
+
+    winningCombos.forEach(function(combo,index){
+
+        if(board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]])
+        winner = board[combo[0]]
+    });
+
+    return winner;
+    
+};
+
+
 function handleTurn(event)
 {
     let idx = squares.findIndex(function(square) {
         return square === event.target;
+
     });
 
 board[idx] = turn;
@@ -29,28 +65,37 @@ else {
     turn = 'X'
 };
 
+win = getWinner();
+
+
 render();
 //console.log(board);
+
 };
 
 function init(){
     board = [
         '', '', '',
-        '', '', '',
-        '', '', ''
-    ];
-
+'', '', '',
+'', '', ''
+];
 render();
-
 };
-
 init();
 
  function render(){
+ 
+
         board.forEach(function(mark, index){
         squares[index].textContent = mark;
-        console.log(mark, index);
-    });
-};
+        //console.log(mark, index);
+        console.log(winner)
+        
+
+        });
+
+        messages.textContent = win ? `${win} 'wins the game!'` : `It's ${turn} 's turn!`;
+
+ };
 
  
